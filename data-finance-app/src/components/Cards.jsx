e4863e085cf7874fe9a10bbbecfbb21f431f1035
joephy527 <joephy527@gmail.com>
2023-09-07 12:48:35 -0700

    updating card details

--- data-finance-app/src/components/Cards.jsx
+++ data-finance-app/src/components/Cards.jsx
@@ -19,23 +19,23 @@ const Cards = () => {
             </div>
             <div className="w-full bg-gray-100 flex flex-col shadow-xl p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
                 <img className="w-20 mx-auto -mt-12 bg-transparent" src={Double} alt="#" />
-                <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
-                <p className="text-center text-4xl font-bold">$149.99</p>
+                <h2 className="text-2xl font-bold text-center py-8">Two Users</h2>
+                <p className="text-center text-4xl font-bold">$199.99</p>
                 <div className="text-center font-medium">
-                    <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-                    <p className="py-2 border-b mx-8">1 Granted User</p>
-                    <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+                    <p className="py-2 border-b mx-8 mt-8">750 GB Storage</p>
+                    <p className="py-2 border-b mx-8">2 Granted User</p>
+                    <p className="py-2 border-b mx-8">Send up to 3 GB</p>
                 </div>
                 <button className="text-white bg-[#ff5349] w-[200px] rounded-md font-medium my-6 mx-auto py-3">Start Trial</button>
             </div>
             <div className="w-full flex flex-col shadow-xl p-4 my-4 rounded-lg hover:scale-105 duration-300">
                 <img className="w-20 mx-auto -mt-12 bg-transparent" src={Triple} alt="#" />
-                <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
-                <p className="text-center text-4xl font-bold">$149.99</p>
+                <h2 className="text-2xl font-bold text-center py-8">Multiple Users</h2>
+                <p className="text-center text-4xl font-bold">$249.99</p>
                 <div className="text-center font-medium">
-                    <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-                    <p className="py-2 border-b mx-8">1 Granted User</p>
-                    <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+                    <p className="py-2 border-b mx-8 mt-8">1 TB Storage</p>
+                    <p className="py-2 border-b mx-8">5 Granted User</p>
+                    <p className="py-2 border-b mx-8">Send up to 5 GB</p>
                 </div>
                 <button className="bg-black text-[#ff5349] w-[200px] rounded-md font-medium my-6 mx-auto py-3">Start Trial</button>
             </div>
