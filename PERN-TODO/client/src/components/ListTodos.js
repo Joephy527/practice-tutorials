7f3282a566a0ff92aedd9e16ff3172e289441bd4
joephy527 <joephy527@gmail.com>
2023-12-04 02:01:45 -0800

    create List component

---
+++ PERN-TODO/client/src/components/ListTodos.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  // delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteAtodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // fetch todo function
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // fetch todos on first page load
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <>
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  onClick={() => deleteTodo(todo.todo_id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ListTodos;
