9adb2568c302f58068b5343744d6bb270a9ac7a9
joephy527 <joephy527@gmail.com>
2023-12-06 12:20:27 -0800

    create api endpoint to get user

---
+++ tweets-api/app/api/users/route.ts
@@ -0,0 +1,18 @@
+import prisma from "@/prisma";
+import { connectToDb } from "@/utils";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  try {
+    await connectToDb();
+
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (err: any) {
+    console.error(err);
+    return NextResponse.json({ err: err.message }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
