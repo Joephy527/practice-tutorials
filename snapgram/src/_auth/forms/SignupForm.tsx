446af635c520414269f287dc83083abfc6544202
joephy527 <joephy527@gmail.com>
2023-12-17 10:56:35 -0800

    finish signup page ui

--- snapgram/src/_auth/forms/SignupForm.tsx
+++ snapgram/src/_auth/forms/SignupForm.tsx
@@ -12,9 +12,12 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
-  const isLoading = true;
+  const isLoading = false;
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -43,7 +46,7 @@ const SignupForm = () => {
         </h2>
 
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use Snapgram enter your details
+          To use Snapgram, please enter your details
         </p>
 
         <form
@@ -108,11 +111,23 @@ const SignupForm = () => {
 
           <Button type="submit" className="shad-button_primary">
             {isLoading ? (
-              <div className="flex-center gap-2">Loading...</div>
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
             ) : (
               "Sign up"
             )}
           </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
