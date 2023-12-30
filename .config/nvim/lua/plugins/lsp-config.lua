42303d17acb50f9e805e6fc747bd4a3c43e0f9e5
joephy527 <joephy527@gmail.com>
2023-12-30 12:49:53 -0800

    update nvim config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -16,18 +16,37 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.prismals.setup({})
-      lspconfig.tailwindcss.setup({})
+
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.dockerls.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.docker_compose_language_service.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.prismals.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilites = capabilities,
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
       vim.keymap.set({ 'n', 'v' }, '<C-a>', vim.lsp.buf.code_action, {})
     end
   }
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,6 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set incsearch")
+vim.cmd("set nohlsearch")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
